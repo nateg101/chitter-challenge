8479d1573a57fe2e7c8c68e3cf5abc064b966cb7
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-10 10:24:21 +0100

    1st commit, gems installed and first feature test passing.

---
+++ spec/features/viewing_chitter_spec.rb
@@ -0,0 +1,7 @@
+
+feature 'Viewing chitter' do
+  scenario 'visting the index page' do
+    visit('/')
+    expect(page).to have_content "Chitter"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
+
+ENV['ENVIRONMENT'] = 'test'
+
+require 'capybara'
+require 'rspec'
+require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
